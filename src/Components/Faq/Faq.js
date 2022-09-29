e2fad33de85e99aca4ebabf9f6a7377f0d0a971d
sidratulshatil <105915771+sidratulshatil@users.noreply.github.com>
2022-09-29 18:27:27 +0600

    First question added

--- src/Components/Faq/Faq.js
+++ src/Components/Faq/Faq.js
@@ -7,9 +7,13 @@ const Faq = () => {
         <div className='faq'>
             <Accordion defaultActiveKey="" flush>
                 <Accordion.Item eventKey="0">
-                    <Accordion.Header>Accordion Item #1</Accordion.Header>
+                    <Accordion.Header>How Does React Work?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        Brought to existence by Jordan Walke and maintained by Facebook, React is the most widely used front-end JavaScript library in the web development domain. Some of the examples of commonly used JavaScript libraries are TensorFlow, Angular, Node etc. React takes a declarative approach to application development that makes it simple to reason about the program while simultaneously aiming for efficiency and flexibility. It is a component-based, open-source front-end library that is exclusively responsible for the application's view layer. It creates basic views for each state in the project, and when the data changes, React updates and renders the appropriate component quickly. The declarative approach simplifies debugging and makes the code more predictable.
+
+                        Let’s have a look at an Instagram page built entirely with React to better understand how it works.
+
+                        React splits the user interface into several components, as seen in the picture, making the code easier to debug. Each component has its attribute and function.
 
                     </Accordion.Body>
                 </Accordion.Item>
